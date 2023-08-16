79dcf6c31efeb5246a0966f1da9f259afcc3ee1e
hcq <1756260160@qq.com>
2023-08-16 23:16:17 +0800

    feat: 修改和删除API

--- backend/mgr/customer.py
+++ backend/mgr/customer.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2023-08-16 22:45:11
-LastEditTime: 2023-08-16 23:00:55
+LastEditTime: 2023-08-16 23:15:35
 FilePath: /Django-backend/backend/mgr/customer.py
 '''
 import json
@@ -65,3 +65,52 @@ def addcustomer(request):
 
 
     return JsonResponse({'ret': 0, 'id':record.id})
+
+# 修改客户信息
+def modifycustomer(request):
+    
+    # 从请求消息中 获取修改客户的信息
+    # 找到该客户，并且进行修改操作
+    customerid = request.params['id']
+    newdata    = request.params['newdata']
+
+    try:
+        # 根据 id 从数据库中找到相应的客户记录<<<<<<<<<
+        customer = Customer.objects.get(id=customerid)
+    except Customer.DoesNotExist:
+        return  {
+            'ret': 1,
+            'msg': f'id 为`{customerid}`的客户不存在'
+        }
+
+
+    if 'name' in  newdata:
+        customer.name = newdata['name']
+    if 'phonenumber' in  newdata:
+        customer.phonenumber = newdata['phonenumber']
+    if 'address' in  newdata:
+        customer.address = newdata['address']
+
+    # 注意，一定要执行save才能将修改信息保存到数据库
+    customer.save()
+
+    return JsonResponse({'ret': 0})
+
+# 删除
+def deletecustomer(request):
+    
+    customerid = request.params['id']
+
+    try:
+        # 根据 id 从数据库中找到相应的客户记录
+        customer = Customer.objects.get(id=customerid)
+    except Customer.DoesNotExist:
+        return  {
+            'ret': 1,
+            'msg': f'id 为`{customerid}`的客户不存在'
+        }
+
+    # delete 方法就将该记录从数据库中删除了
+    customer.delete()
+
+    return JsonResponse({'ret': 0})
