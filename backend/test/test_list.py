2af789073a4c3d82223f53814c3f9149d2084898
hcq <1756260160@qq.com>
2023-08-17 03:16:27 +0800

    feat: 测试用例

--- backend/test/test_list.py
+++ backend/test/test_list.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2023-08-17 03:10:28
-LastEditTime: 2023-08-17 03:14:37
+LastEditTime: 2023-08-17 03:16:19
 FilePath: /Django-backend/backend/test/test_list.py
 '''
 # 测试登陆
@@ -13,7 +13,7 @@ params = {
     'action': 'list_customer',
 }
 response = requests.get('http://localhost:8000/api/mgr/customers',
-                         params==params)
+                         params=params)
 pprint.pprint(response.json())
 
 
