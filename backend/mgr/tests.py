a450335cc408837d239c3e3d39cf040ccca2ac19
hcq <1756260160@qq.com>
2023-08-17 03:08:34 +0800

    feat: 测试用例

--- backend/mgr/tests.py
+++ backend/mgr/tests.py
@@ -4,19 +4,9 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2023-08-16 22:35:59
-LastEditTime: 2023-08-17 02:58:58
+LastEditTime: 2023-08-17 03:02:48
 FilePath: /Django-backend/backend/mgr/tests.py
 '''
 from django.test import TestCase
 
 # Create your tests here.
-
-# 测试登陆
-import requests, pprint
-payload = {
-    'username': 'byhy',
-    'password': '88888888'
-}
-response = requests.post('http://localhost/api/mgr/signin',
-                         data=payload)
-pprint.pprint(response.json())
---
+++ backend/test/test_login.py
@@ -0,0 +1,19 @@
+'''
+Description: 测试登陆
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2023-08-17 03:02:39
+LastEditTime: 2023-08-17 03:02:51
+FilePath: /Django-backend/backend/test/test_login.py
+'''
+
+# 测试登陆
+import requests, pprint
+payload = {
+    'username': 'byhy',
+    'password': '88888888'
+}
+response = requests.post('http://localhost/api/mgr/signin',
+                         data=payload)
+pprint.pprint(response.json())
